1f52b5a8a695e173e1a88eececa145852a040fee
Idzikowski20 <patryk.idzikowski@interia.pl>
2024-09-07 21:35:29 +0200

    zmiana hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,44 +7,25 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`pt-10 absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`pt-10 absolute inset-0 top-[200px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
-        <div>
+        <div className="w-21">
           <h1 className={`${styles.heroHeadText} text-white`}>
             Global<span className='text-[#915EFF]'>Zone</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Stawiamy na ekologie!<br className='sm:block hidden' />
-            Tekst przykładowy.
+            Stawiamy na ekologię!<br className='sm:block hidden' />
+            Jesteśmy firmą importującą najwyższej jakości towar, <br>
+            </br>który jest w 100% ekologiczny dla planety.
           </p>
         </div>
       </div>
-
-      
       <Spline scene="https://prod.spline.design/rNKQ4evbTav4znyU/scene.splinecode" />
-
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </a>
-      </div>
     </section>
   );
 };
